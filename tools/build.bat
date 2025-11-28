d44174fd634f2df139b378758641208880b70d2c
KiG Organization <misteranthems5@gmail.com>
2025-11-28 21:50:15 +0500

    Update build.bat

--- tools/build.bat
+++ tools/build.bat
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the QuarkBIOS project.
+ *
+ * Copyright (C) 2025 KiG Organization
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 @echo off
 REM Build script for QuarkBIOS
 
@@ -8,5 +27,6 @@ if %errorlevel% neq 0 (
     echo Assembly failed!
     exit /b %errorlevel%
 )
+
 
 echo Build successful! Output: ..\build\boot.bin
