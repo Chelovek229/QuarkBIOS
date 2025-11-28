51e9852d70d3e274e22973e90b6f84773e53f39d
KiG Organization <misteranthems5@gmail.com>
2025-11-28 21:46:43 +0500

    Update bios.h

--- src/bios.h
+++ src/bios.h
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the QuarkBIOS project.
+ *
+ * Copyright (C) 2025 KiG Organization
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 // bios.h - Common definitions for QuarkBIOS
 
 #ifndef BIOS_H
@@ -14,5 +34,6 @@ void init_mouse();
 void get_mouse_position(int* x, int* y);
 
 void read_sector(int drive, int sector, void* buffer);
+
 
 #endif // BIOS_H
