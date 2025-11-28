7a79cd4225be3e9a74c46620fa623195ea549309
KiG Organization <misteranthems5@gmail.com>
2025-11-28 21:47:23 +0500

    Update gui.c

--- src/gui.c
+++ src/gui.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the QuarkBIOS project.
+ *
+ * Copyright (C) 2025 KiG Organization
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // gui.c - Graphical User Interface for QuarkBIOS
 
 #include "bios.h"
@@ -26,4 +45,5 @@ void draw_text(int x, int y, const char* text, int color) {
         text++;
         x += 8; // Move to the next character position
     }
+
 }
